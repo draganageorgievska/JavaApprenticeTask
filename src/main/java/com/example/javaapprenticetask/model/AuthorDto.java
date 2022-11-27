84bf2163cc97ed89c314e12a4acc3288f7d03ffb
Dragana <georgievskad@yahoo.com>
2022-11-27 02:19:06 +0100

    changed type and name od date variable

--- src/main/java/com/example/javaapprenticetask/model/AuthorDto.java
+++ src/main/java/com/example/javaapprenticetask/model/AuthorDto.java
@@ -1,18 +1,17 @@
 package com.example.javaapprenticetask.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 @Data
 public class AuthorDto {
     private String firstName;
     private String lastName;
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Date dateOfBirth;
-    public AuthorDto(String firstName,String lastName, Date dateOfBirth){
+    public AuthorDto(String firstName,String lastName, String dateOfBirth) throws ParseException {
         this.firstName=firstName;
         this.lastName=lastName;
-        this.dateOfBirth=dateOfBirth;
+        this.dateOfBirth=new SimpleDateFormat("yyyy-MM-dd").parse(dateOfBirth);
     }
 }
